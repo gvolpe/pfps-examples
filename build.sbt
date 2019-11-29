6ae220d8d225a6c1944438bf9158828b2202f76d
Gabriel Volpe <volpegabriel@gmail.com>
2019-11-29 10:38:43 -0300

    updating a bunch of stuff

--- build.sbt
+++ build.sbt
@@ -1,22 +1,23 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.13.0"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "dev.profunktor"
+ThisBuild / scalaVersion := "2.13.0"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "dev.profunktor"
 ThisBuild / organizationName := "ProfunKtor"
 
 lazy val root = (project in file("."))
   .settings(
     name := "examples",
     scalacOptions += "-Ymacro-annotations",
+    scalafmtOnCompile := true,
     libraryDependencies ++= Seq(
-      compilerPlugin(Libraries.kindProjector),
-      compilerPlugin(Libraries.betterMonadicFor),
-      Libraries.cats,
-      Libraries.catsEffect,
-      Libraries.console4cats,
-      Libraries.fs2,
-      Libraries.newtype,
-      Libraries.refinedCore
-    )
+          compilerPlugin(Libraries.kindProjector),
+          compilerPlugin(Libraries.betterMonadicFor),
+          Libraries.cats,
+          Libraries.catsEffect,
+          Libraries.console4cats,
+          Libraries.fs2,
+          Libraries.newtype,
+          Libraries.refinedCore
+        )
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,24 +3,24 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val cats          = "2.0.0"
-    val catsEffect    = "2.0.0"
-    val console4cats  = "0.8.0"
-    val fs2           = "2.0.1"
-    val log4cats      = "1.0.0"
-    val newtype       = "0.4.3"
-    val refined       = "0.9.10"
+    val cats         = "2.0.0"
+    val catsEffect   = "2.0.0"
+    val console4cats = "0.8.0"
+    val fs2          = "2.1.0"
+    val log4cats     = "1.0.0"
+    val newtype      = "0.4.3"
+    val refined      = "0.9.10"
 
-    val betterMonadicFor = "0.3.0"
+    val betterMonadicFor = "0.3.1"
     val kindProjector    = "0.10.3"
     val logback          = "1.2.1"
   }
 
   object Libraries {
-    lazy val cats         = "org.typelevel"  %% "cats-core"     % Versions.cats
-    lazy val catsEffect   = "org.typelevel"  %% "cats-effect"   % Versions.catsEffect
-    lazy val console4cats = "dev.profunktor" %% "console4cats"  % Versions.console4cats
-    lazy val fs2          = "co.fs2"         %% "fs2-core"      % Versions.fs2
+    lazy val cats         = "org.typelevel"  %% "cats-core"    % Versions.cats
+    lazy val catsEffect   = "org.typelevel"  %% "cats-effect"  % Versions.catsEffect
+    lazy val console4cats = "dev.profunktor" %% "console4cats" % Versions.console4cats
+    lazy val fs2          = "co.fs2"         %% "fs2-core"     % Versions.fs2
 
     lazy val refinedCore = "eu.timepit" %% "refined"      % Versions.refined
     lazy val refinedCats = "eu.timepit" %% "refined-cats" % Versions.refined
