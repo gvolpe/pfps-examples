02662e8bd74c8e37f7749fd17680ad00caeba9a3
Gabriel Volpe <volpegabriel@gmail.com>
2019-12-21 10:55:12 -0300

    StateT example

--- src/main/scala/examples/mtl/state.scala
+++ src/main/scala/examples/mtl/state.scala
@@ -1,10 +1,12 @@
 package examples.mtl
 
+import cats.data.StateT
 import cats.effect._
 import cats.effect.Console.implicits._
 import cats.effect.concurrent.Ref
 import cats.implicits._
 import cats.mtl._
+import cats.mtl.instances.all._
 import com.olegpy.meow.effects._
 import com.olegpy.meow.hierarchy._
 import org.manatki.derevo.catsInstances.show
@@ -12,12 +14,14 @@ import org.manatki.derevo.derive
 
 object StateDemo extends IOApp {
 
+  implicit val stConsole = SyncConsole.stdio[StateT[IO, FooState, *]]
+
   @derive(show)
   case class FooState(value: String) extends AnyVal
 
   import com.olegpy.meow.prelude._ // Use Monad instance from MonadState
 
-  def mtlProgram[F[_]: Console](implicit M: MonadState[F, FooState]): F[Unit] =
+  def program[F[_]: Console](implicit M: MonadState[F, FooState]): F[Unit] =
     for {
       current <- M.get
       _ <- Console[F].putStrLn(current)
@@ -26,13 +30,17 @@ object StateDemo extends IOApp {
       _ <- Console[F].putStrLn(updated)
     } yield ()
 
-  val program = Ref.of[IO, FooState](FooState("bar")).flatMap { ref =>
-    ref.runState { implicit ms =>
-      mtlProgram[IO]
+  val p1: IO[Unit] =
+    program[StateT[IO, FooState, *]].run(FooState("mt")).void
+
+  val p2: IO[Unit] =
+    Ref.of[IO, FooState](FooState("bar")).flatMap { ref =>
+      ref.runState { implicit ms =>
+        program[IO]
+      }
     }
-  }
 
   def run(args: List[String]): IO[ExitCode] =
-    program.as(ExitCode.Success)
+    p2.as(ExitCode.Success)
 
 }
