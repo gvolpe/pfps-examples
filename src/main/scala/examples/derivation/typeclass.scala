5c10386f144452a2f0cab777148804a8a6619912
Gabriel Volpe <volpegabriel@gmail.com>
2019-12-24 10:54:26 -0300

    fixing imports + more derivation stuff

--- src/main/scala/examples/derivation/typeclass.scala
+++ src/main/scala/examples/derivation/typeclass.scala
@@ -2,22 +2,33 @@ package examples.derivation
 
 import cats._
 import cats.implicits._
-import org.manatki.derevo.catsInstances.show
+import cats.tagless.FunctorK
+import org.manatki.derevo.catsInstances.{ eq => eqv, show, semigroup }
 import org.manatki.derevo.derive
-import org.manatki.derevo.tagless.{ flatMap, functor }
+import org.manatki.derevo.tagless.{ flatMap, functor, functorK }
 
 object typeclass {
+
+  @derive(functorK)
+  sealed trait Foo[F[_]]
+  case object FooOne extends Foo[List]
+  case object FooTwo extends Foo[Option]
+
+  FunctorK[Foo]
 
   @derive(functor, flatMap)
   sealed trait HigherKind[A]
   case object KindOne extends HigherKind[Int]
   case object KindTwo extends HigherKind[String]
 
-  @derive(show)
-  final case class SingleKind(value: String)
-
   Functor[HigherKind]
   FlatMap[HigherKind]
-  //Monad[HigherKind] // does not compile
+
+  @derive(eqv, semigroup, show)
+  final case class SingleKind(value: String)
+
+  Eq[SingleKind]
+  Semigroup[SingleKind]
+  Show[SingleKind]
 
 }
