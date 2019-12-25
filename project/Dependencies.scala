7d84f95f041c730f028d9a0a16076175c769474a
Gabriel Volpe <volpegabriel@gmail.com>
2019-12-25 13:28:55 -0300

    closes #15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val cats         = "2.0.0"
+    val cats         = "2.1.0"
     val catsEffect   = "2.0.0"
     val catsMeowMtl  = "0.4.0"
     val console4cats = "0.8.1"
