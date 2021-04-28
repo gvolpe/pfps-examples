ea25a8b6ba670099a7d08491b2ee0de232e5d651
Scala Steward <me@scala-steward.org>
2021-04-28 18:36:39 +0200

    Update derevo-cats, derevo-cats-tagless, ... to 0.12.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val cats       = "2.6.0"
     val catsEffect = "3.1.0"
     val catsMtl    = "1.2.0"
-    val derevo     = "0.12.2"
+    val derevo     = "0.12.5"
     val fs2        = "3.0.2"
     val monocle    = "3.0.0-M5"
     val newtype    = "0.4.4"
